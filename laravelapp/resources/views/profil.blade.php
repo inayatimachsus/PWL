155d03a0b68a041d72f288a9e6bac3349f71c970
inayatimachsus <56281888+inayatimachsus@users.noreply.github.com>
2020-04-07 10:38:22 +0700

    Jobsheet 9

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Profil Saya</title>
+</head>
+<body>
+<h1>Profil Saya</h1>
+<p>Perkenalkan  Nama  saya  Inayati Machsus Izza Addin,  saya  seorang  Mahasiswa rajin. AAMIIN.
+.</p>
+</body>
+</html>
